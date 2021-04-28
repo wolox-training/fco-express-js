0ae9477abbb20d33a27277c4da06eaa98b38a449
Fredy Cortes <fredy.cortes@wolox.co>
2021-04-28 16:39:25 -0500

    [FIX]: so much nesting at config props and external service error handling

--- app/services/weets.js
+++ app/services/weets.js
@@ -1,14 +1,16 @@
 const { default: Axios } = require('axios');
-const logger = require('../logger');
 
 const config = require('../../config');
+const errors = require('../errors');
+const logger = require('../logger');
 
 exports.getQuote = async () => {
   try {
-    const { data } = await Axios.get(config.common.api.randomWeetsApi);
+    const { data } = await Axios.get(config.common.geekJokesApi);
+    logger.info(data);
     return data;
   } catch (error) {
     logger.error(error);
-    return error;
+    throw errors.externalApiError('Have problems with geek jokes external service');
   }
 };
--- config/index.js
+++ config/index.js
@@ -34,8 +34,7 @@ const config = {
     api: {
       bodySizeLimit: process.env.API_BODY_SIZE_LIMIT,
       parameterLimit: process.env.API_PARAMETER_LIMIT,
-      port: process.env.PORT,
-      randomWeetsApi: process.env.API_RANDOM_WEETS
+      port: process.env.PORT
     },
     session: {
       header_name: 'authorization',
@@ -45,7 +44,8 @@ const config = {
       apiDate: process.env.API_DATE || 'X-API-Date',
       packageVersion: process.env.PACKAGE_VERSION || 'X-Package-Version',
       nodeVersion: process.env.NODE_VERSION || 'X-Node-Version'
-    }
+    },
+    geekJokesApi: process.env.API_RANDOM_WEETS
   }
 };
 
