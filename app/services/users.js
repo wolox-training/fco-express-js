fc6d0963b5fc3c309e2cd1512634ad0d898ef946
Fredy Cortes <fredy.cortes@wolox.co>
2021-05-03 15:50:43 -0500

    [FIX]: handling error without call the next function and responsability of users service

--- app/services/users.js
+++ app/services/users.js
@@ -1,8 +1,6 @@
 const { databaseError } = require('../errors');
 const logger = require('../logger');
 const { user: UserModel } = require('../models');
-const { signUpSerializer } = require('../serializers/users');
-const { mapToSerializer } = require('../utils/objects');
 
 const loggerPath = 'service:users';
 
@@ -10,7 +8,7 @@ exports.signUp = async userData => {
   try {
     const createdUser = await UserModel.create(userData);
     logger.info(`${loggerPath}:signUp:username: ${createdUser.name}`);
-    return mapToSerializer(createdUser, signUpSerializer);
+    return createdUser;
   } catch (error) {
     logger.error(`${loggerPath}:signUp:database: ${error.message}`);
     throw databaseError('database error in SignUp');
