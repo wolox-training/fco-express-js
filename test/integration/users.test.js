9e5cfaf83985c13e0cc8168195a2fa87bfa641cb
Fredy Cortes <fredy.cortes@wolox.co>
2021-05-03 13:44:25 -0500

    [ADD]: init testing using jest

---
+++ test/integration/users.test.js
@@ -0,0 +1,11 @@
+// const request = require('supertest');
+
+// const app = require('../../app');
+
+// describe('users endpoints', () => {
+//   describe('signUp endpoint', () => {
+//     xtest('should be truthy', () => {
+//       expect(true).toBeTruthy();
+//     });
+//   });
+// });
