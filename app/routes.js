9f1470617a1247077fbc228bb75ee121eab210dc
Fredy Cortes <fredy.cortes@wolox.co>
2021-05-07 17:34:20 -0500

    [FIX]: validation of the existence of the email  in controller

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,13 @@ const { healthCheck } = require('./controllers/healthCheck');
 const { signUp, signIn } = require('./controllers/users');
 const { signUpDto, signInDto } = require('./dtos/users');
 const { mapSnakeToCamel } = require('./middlewares/mappers');
+const { existsEmail } = require('./middlewares/users');
 const { validationSchema } = require('./middlewares/validationSchema');
 
 exports.init = app => {
   app.get('/health', healthCheck);
 
   // user endpoints
-  app.post('/users', [signUpDto, validationSchema, mapSnakeToCamel], signUp);
+  app.post('/users', [signUpDto, validationSchema, existsEmail, mapSnakeToCamel], signUp);
   app.post('/users/sessions', [signInDto, validationSchema], signIn);
 };
