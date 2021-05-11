794d25f1c638d8e9dd2e82136700a2a795a1422e
Fredy Cortes <fredy.cortes@wolox.co>
2021-05-11 13:08:24 -0500

    [ADD]: weet model and depending relationship with user

---
+++ migrations/migrations/20210511160040-weetModel.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('weets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('weets');
+  }
+};
